ee0b1b7f7b0c867c45be9af3f7a28e704891e5cb
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 15:42:09 +0200

    try and catch block

--- djs10/src/App.jsx
+++ djs10/src/App.jsx
@@ -6,7 +6,23 @@ function App() {
   const [posts, setPosts] = React.useState([]) // initialise state for  blog posts
   const [error, setError] = React.useState(null) //  initialise state for  error messages
 
-  React.useEffect(()=>{   
+  React.useEffect(()=>{    //set use effect hook for handling api calls
+    async function addPosts(){
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data = await response.json()
+        console.log(data)
+
+        if (!response.ok) {
+            throw Error("Something went wrong, check resource")
+        }
+
+      } catch (err) {
+        setError(err)
+        
+      }
+
+    }
 
 
   }, [])
