6644427be51df1e246a8779705a3c78700dfdf40
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 15:11:03 +0200

    init use state hook for posts and errors

--- djs10/src/App.jsx
+++ djs10/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState,useEffect } from 'react'
+import React from 'react'
 
 
 function App() {
+  const [posts, setPosts] = React.useState([]) // initialise state for  blog posts
+  const [error, setError] = React.useState(null) //  initialise state for  error messages
 
+
+
+  
   return (
    <h1>Posts</h1>
   
